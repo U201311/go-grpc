61f8d6fb884ef1a207808d3d8c979f1696461f13
liying <yingli950906@163.com>
2023-01-17 10:57:38 +0800

    golang-grpc demo

---
+++ server/simple_server/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "go-grpc-example/proto"
+
+	"google.golang.org/grpc"
+)
+
+type SearchService struct {
+	pb.UnimplementedSearchServiceServer
+}
+
+func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.SearchResponse, error) {
+	return &pb.SearchResponse{Response: r.GetRequest() + " Server"}, nil
+}
+
+const PORT = "9009"
+
+func main() {
+	server := grpc.NewServer()
+	pb.RegisterSearchServiceServer(server, &SearchService{})
+
+	lis, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v", err)
+	}
+
+	server.Serve(lis)
+}
